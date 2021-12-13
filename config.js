3ee48037714443e8fc8256db5845bed9c09f3e5a
Reallove1225 <96060048+Reallove1225@users.noreply.github.com>
2021-12-13 20:47:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cho tình iu của tớ",
+    introTitle: 'Nguyễn Hoài Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Cậu có thích tớ ko ?  ',
+    btnYes: 'Thích lắm lun',
+    btnNo: 'Cậu mơ à',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Với tớ yêu cậu ko cần lí do',
+    mess: 'Tớ biết mà 🥰. Yêu cậu 3000 😘😘',
+    messDesc: 'Noel đi chơi với tớ nhaa ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: ''https://www.facebook.com/messages/t/100005712460502' 
